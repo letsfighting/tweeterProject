c818991d79557310ad0c5e10a015f15a860bce3e
Elroy Hui <huielroy@hotmail.com>
2022-03-30 17:04:54 -0400

    +++ajax post and get requests. tweets populate into containers. refactored hover event listener. time ago conversion

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,76 @@
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+  for (const post of tweets) {
+    const $tweet = createTweetElement(post);
+    //console.log($tweet);
+    $("#tweets-container").append($tweet);
+  }
+};
+
+const createTweetElement = function (tweet) {
+  const tweetElement = `<article class="tweet">
+    <header>
+    <span>
+    <img src=${tweet.user.avatars} alt=${tweet.user.name}>
+    </span>
+    <span class="name">${tweet.user.name}</span>
+    </header>
+
+    <p>${tweet.content.text}</p>
+
+    <footer>
+      <span id="days-ago">${timeago.format(tweet.created_at)}</span>
+      <span id="fn-btns">
+        <i class="fa-solid fa-flag opt-btns"></i>
+        <i class="fa-solid fa-retweet opt-btns"></i>
+        <i class="fa-solid fa-heart opt-btns"></i>
+      </span>
+    </footer>
+  </article>`;
+
+  return tweetElement;
+};
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const loadTweets = function () {
+  $.ajax({
+    url: `/tweets/`,
+    method: "GET",
+  }).then(function (response) {
+    console.log("getmethod resp:", response);
+    //renders the tweets
+    renderTweets(response);
+  });
+};
+
 $(document).ready(function () {
-  $("article.tweet").mouseover(function (event) {
-    $(this).addClass("hovered");
+  loadTweets();
+
+  $("#form-submit").submit(function (event) {
+    event.preventDefault();
+    const tweetPost = $(this).serialize();
+
+    $.ajax({
+      url: `/tweets/`,
+      method: "POST",
+      data: tweetPost,
+    }).then(function (response) {
+      console.log("postmethod resp: ", response);
+      loadTweets();
+    });
   });
 
-  $("article.tweet").mouseout(function (event) {
-    $(this).removeClass("hovered");
-  });
-
+  /* to add hover color changes to buttons and boxshadows to hovered tweets - replaced with css id/class/element:hover method
   $("i.opt-btns").mouseover(function (event) {
     $(this).addClass("hoveredbtn");
   });
@@ -20,4 +78,12 @@ $(document).ready(function () {
   $("i.opt-btns").mouseout(function (event) {
     $(this).removeClass("hoveredbtn");
   });
+  
+  $("article.tweet").mouseover(function (event) {
+    $(this).addClass("hovered");
+  });
+
+  $("article.tweet").mouseout(function (event) {
+    $(this).removeClass("hovered");
+  });*/
 });
